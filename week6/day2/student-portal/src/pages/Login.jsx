a3da5038d4c1da60f3b9c21f815dc8b72d54b938
Dilovan Matini <dilovan@lelav.com>
2026-06-23 10:09:11 +0300

    Update Login.jsx

--- week6/day2/student-portal/src/pages/Login.jsx
+++ week6/day2/student-portal/src/pages/Login.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+
 function Login () {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    function handleLogin () {
+        if (email === "" || password === ""){
+            setError("Please fill in all fields");
+        } else {
+            setError("");
+        }
+    }
+
     return (
         <div className="p-5">
             <h1 className="text-2xl">Login</h1>
+            {error && <p className="text-red-500">{error}</p>}
             <input
                 type="email"
                 placeholder="Email"
                 className="block p-2 mt-3 border w-64"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
             <input
                 type="password"
                 placeholder="Password"
                 className="block p-2 mt-3 border w-64"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
-            <button className="bg-blue-900 text-white p-2 mt-3">Login</button>
+            <button onClick={handleLogin} className="bg-blue-900 text-white p-2 mt-3">Login</button>
         </div>
     );
 }
